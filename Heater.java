a5e67ab58a29eac01fda6e64f1be6aabce742d69
pablo salazar riou <ryuuuu456@gmail.com>
2026-01-28 14:52:00 -0600

    Ha sido creada la clase heater con el atriuto temperature

---
+++ Heater.java
@@ -0,0 +1,12 @@
+
+/**
+ * Write a description of class Heater here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class Heater
+{
+    private float temperature;
+    
+}
